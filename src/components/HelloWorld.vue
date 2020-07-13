30b749519f9509ddb70e72ded3123fd0a9d2c814
yanwei8095 <yanwei2007095@qq.com>
2020-07-13 14:45:31 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: 'hello world'
+    }
+  }
+}
+</script>
+<style lang='stylus' rel='stylesheet/stylus' scoped>
+h2 {
+  color: red;
+}
+</style>
